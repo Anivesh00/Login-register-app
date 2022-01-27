ea93f14a77ba1ebee63b13bbfb8d9f69b217c608
Anivesh Singh <singhanivesh0@gmail.com>
2022-01-27 13:08:33 +0530

    validation in password and email

--- login-register-app/src/components/register/register.js
+++ login-register-app/src/components/register/register.js
@@ -2,9 +2,9 @@ import React, { useState } from "react"
 import "./register.css"
 import axios from "axios" //for API calling
 import { useNavigate  } from "react-router-dom";
+import validator from 'validator'
 
 const Register  = () => {
-
     const navigate = useNavigate()
 
     const [ user, setUser] = useState({
@@ -28,18 +28,21 @@ const Register  = () => {
 
     const register = () => {
         const { name, email, password, reEnterPassword, dob, gender } = user
-        if( name && email && password && dob && gender && (password === reEnterPassword)){
-            // alert("posted")
+        if( name && email && password && dob && gender && 
+            (validator.isEmail(email))
+             (validator.isStrongPassword(password, {
+                minLength: 8, minLowercase: 1,
+                minUppercase: 1, minNumbers: 1, minSymbols: 1
+              })) && (password === reEnterPassword)){
             axios.post("http://localhost:9002/register", user)
             .then( res =>
-                //  console.log(res)
             {
                 alert(res.data.message)
                 navigate("/login")
             }
             )
         } else {
-            alert("invalid input")
+            alert("invalid input! Please check the password and email ")
         }
         
     }
@@ -50,17 +53,12 @@ const Register  = () => {
             <h1>Register</h1>
             <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={ handleChange }></input>
             <input type="email" name="email" value={user.email} placeholder="Your Email" onChange={ handleChange }></input>
-            <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={ handleChange }></input>
+            <input type="password"  name="password" value={user.password} placeholder="Your Password" onChange={handleChange}></input>
+            <p id="h" hidden> Hello world</p>
             <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-enter Password" onChange={ handleChange }></input>
             <span>DOB: &nbsp;</span>
             <input type="text" placeholder="DD/MM/YYYY" name="dob" value={user.dob} onChange={ handleChange }/>
             <span>Gender: &nbsp;</span>
-            {/* <label htmlFor="male" className="gender">&nbsp;Male&nbsp;
-            <input type="radio" id="male" name="gender" value={user.male} onChange={ handleChange }/></label>
-            <label htmlFor="female" className="gender">Female&nbsp;
-            <input type="radio" id="female" name="gender" value={user.female} onChange={ handleChange }/></label>
-            <label htmlFor="nb" className="gender">Non-binary&nbsp;
-            <input type="radio" id="nb" name="gender" value={user.nb} onChange={ handleChange }/></label> */}
             <select name="gender" onChange={ handleChange } value={user.gender} >
                 <option value=" "> Select.... </option>
                 <option value="male"> Male </option>
@@ -73,4 +71,5 @@ const Register  = () => {
         </div>
     )
 }
+
 export default Register
